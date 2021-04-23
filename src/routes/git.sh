2163c99ed8d1b76814eb8a43bba1f3b0ab95feb4
wxw007 <359443034@qq.com>
2021-04-23 19:02:10 +0800

    提交

---
+++ src/routes/git.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo '开始拉取最新代码'
+
+cd /Users/hupimao/Desktop/learn/node-dev/client
+#git pull
+#echo '代码拉取完成'
+npm run build
+echo '准备打包'
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,29 +1,26 @@
-const router = require('koa-router')()
-const child_process = require('child_process')
+const router = require('koa-router')();
+const exec = require('child_process').execSync;
 
 router.get('/', async (ctx, next) => {
-  ctx.body = {
-    a:1,
-    b:2
-  }
-})
+    ctx.body = {
+        a:1,
+        b:2,
+    };
+});
 
 router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-  child_process.exec('git pull', {cwd:githref}, function (error, stdout, stderr) {
-    if (error !== null) {
-      console.log('exec error: ' + error);
-    }else{
-      console.log(time+' '+ stdout)
-      // console.log(stdout)
-    }
-  })
-})
+    exec('bash /Users/hupimao/Desktop/learn/xiaojukoa2/src/routes/git.sh', (err, stdout, stderr) => {
+      console.log('stdout', stdout)
+    });
+    ctx.body = '脚本执行完毕';
+
+    
+});
 
 router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
-})
+    ctx.body = {
+        title: 'koa2 json',
+    };
+});
 
-module.exports = router
+module.exports = router;
