9cb4fcadf64334ec534ee89d17f1fc7ed2b48ddd
wxw007 <359443034@qq.com>
2021-04-25 19:11:29 +0800

    提交

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,16 +10,26 @@ router.get('/', async (ctx, next) => {
 
 router.get('/string', async (ctx, next) => {
     exec('bash /Users/hupimao/Desktop/learn/xiaojukoa2/src/routes/git.sh', (err, stdout, stderr) => {
+      console.log('err', err)
       console.log('stdout', stdout)
+      console.log('stderr', stderr)
     });
     ctx.body = '脚本执行完毕';
 
     
 });
 
+let a = (function() {
+    let n = 0;
+    return function () {
+        return ++n
+    }
+})()
+
 router.get('/json', async (ctx, next) => {
+    let sum = a()
     ctx.body = {
-        title: 'koa2 json',
+        title: sum,
     };
 });
 
